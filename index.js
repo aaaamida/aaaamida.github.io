d6151d74686813e8d91011bd08acbe4916d8fb2e
aaaamida <aaaamida6747@gmail.com>
2024-11-06 00:23:11 +0800

    feature: added list of submissions as table

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const read_more = document.getElementById("read_more");
-const extra = document.getElementsByClassName("extra");
-
+const extras = document.getElementsByClassName("extra");
+        
 read_more.addEventListener("click", () => {
-        switch (extra.style.display) {
-                case "none":
-                        extra.style.display = "block";
-                        read_more.textContent = "Read Less";
-                        break;
-                default:
-                        extra.style.display = "none"
-                        read_more.textContent = "Read More"
-                        break;
-        }
+        Array.from(extras).forEach(extra => {
+                switch (extra.style.display) {
+                        case "none":
+                                extra.style.display = "block";
+                                read_more.hidden = true;
+                                break;
+                        default:
+                                extra.style.display = "none"
+                                read_more.textContent = "Read More"
+                                break;
+                }
+        })                                
 })
